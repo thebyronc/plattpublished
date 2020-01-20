4d80bd14838a4b303048e4c538c89b532f770db9
thebyronc <thebyronc@gmail.com>
2020-01-20 12:51:40 -0800

    initial

---
+++ component---src-pages-404-jsx-93824441253fa3d9af08.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{pssB:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),s=n.n(a),u=n("L6Je"),l=n("vrFN");t.default=function(){return s.a.createElement(u.a,null,s.a.createElement(l.a,{title:"404: Not found"}),s.a.createElement("h1",null,"NOT FOUND"),s.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-93824441253fa3d9af08.js.map
