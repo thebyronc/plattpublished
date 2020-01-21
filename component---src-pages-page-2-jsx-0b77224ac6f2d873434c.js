413084ba7706bff62fe5a26debd5557b3a008474
thebyronc <thebyronc@gmail.com>
2020-01-20 18:01:19 -0800

    add sw

---
+++ component---src-pages-page-2-jsx-0b77224ac6f2d873434c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{caWp:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),c=a("Wbzz"),l=a("L6Je"),r=a("vrFN");t.default=function(){return o.a.createElement(l.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-jsx-0b77224ac6f2d873434c.js.map
